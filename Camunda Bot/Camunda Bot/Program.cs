98bffaab0d413195d26c60f1fe1ee06cfb2bc27a
Jake Ralph <jralph23@hotmail.com>
2020-03-05 10:01:13 +1000

    Added Activity Class, and the GetActivities() function to the CamundaBot class (which currently doesnt work correctly)

--- Camunda Bot/Camunda Bot/Program.cs
+++ Camunda Bot/Camunda Bot/Program.cs
@@ -3,9 +3,40 @@ using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace CamundaBot
 {
+
+
+    class Activity
+    {
+        public string id { get; set; }
+        public string parentActivityInstanceId { get; set; }
+        public string activityId { get; set; }
+        public string activityName { get; set; }
+        public string activityType { get; set; }
+        public string processDefinitionKey { get; set; }
+        public string processDefinitionId { get; set; }
+        public string processInstanceId { get; set; }
+        public string executionId { get; set; }
+        public string taskId { get; set; }
+        public string calledProcessInstanceId { get; set; }
+        public string calledCaseInstanceId { get; set; }
+        public string assignee { get; set; }
+        public string canceled { get; set; }
+        public string completeScope { get; set; }
+        public string tenantId { get; set; }
+        public string removalTime { get; set; }
+        public string rootProcessInstanceId { get; set; }
+
+        public int startTime { get; set; }
+        public int endTime { get; set; }
+        public int durationInMillis { get; set; }
+    }
+
+
 
 
     class CamundaBot
@@ -38,17 +69,36 @@ namespace CamundaBot
             return result;
         }
 
-        public async Task<string> GetActivities()
+        public async Task<List<Activity>> GetActivities()
         {
             string url = "http://localhost:8080/engine-rest/history/activity-instance?processDefinitionId=" + this.definitionID;
 
             HttpResponseMessage response = await this.client.GetAsync(url);
 
-            string activities = response.Content.ReadAsStringAsync().Result;
+            MatchCollection activities = Regex.Matches(response.Content.ReadAsStringAsync().Result, @"{[^{}]*}");
 
-            return activities;
+
+            for (int i = 0; i < activities.Length - 1; i++)
+            {
+                activities[i] = activities[i] + "}";
+            }
+
+            Console.WriteLine(activities[0]);
+
+            List<Activity> activityList = new List<Activity>();
+
+            /*
+            foreach (string activity in activities)
+            {
+                activityList.Add(JsonConvert.DeserializeObject<Activity>(activity));
+            }
+            */
+
+            return activityList;
         }
 
+
+        
     
     }
 
@@ -58,17 +108,22 @@ namespace CamundaBot
         static async Task Main(string[] args)
         {
             string definitionKey = "sequence";
-            string definitionID = "sequence:1:cf32a197-5dbf-11ea-a86b-00d86175334f";
+            string definitionID = "sequence:1:56ede483-5de0-11ea-a86b-00d86175334f";
             string json = "";
             CamundaBot sequenceBot = new CamundaBot(definitionKey, definitionID, json);
 
 
             // This should print the response from Camunda, Just like what is returned to Postman.
-            string result = await sequenceBot.StartProcess();
-            Console.WriteLine(result);
+            //string result = await sequenceBot.StartProcess();
+            //Console.WriteLine(result);
 
-            string activities = await sequenceBot.GetActivities();
-            Console.WriteLine(activities);
+            List<Activity> activities = await sequenceBot.GetActivities();
+            
+
+            //Console.WriteLine(activities[0].activityName);
+
+
+            //Console.WriteLine(activitiesJSON);
 
             
         }
